10bc1ad262e27268ff730d749fa9e0c30a82c589
ChRatchapol <50233981+ChRatchapol@users.noreply.github.com>
2021-01-22 23:22:27 +0700

    add draft route to app.py

--- project/app.py
+++ project/app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session
+import datetime
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return "login"
+
+
+@app.route("/balance", methods=["PUT", "GET"])
+def balance():
+    return "balance"
+
+
+@app.route("/lib-store", methods=["PUT", "GET"])
+def lib_store():
+    return "lib. store"
+
+
+@app.route("/price-cal")
+def price_cal():
+    return "price cal."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
