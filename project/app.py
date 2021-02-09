766d76c96d29336035a78b403e00e63ceaca54b4
ChRatchapol <50233981+ChRatchapol@users.noreply.github.com>
2021-02-09 19:48:47 +0700

    finish login v1

--- project/app.py
+++ project/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request, session, make_response, redirect, flash
 import datetime
+import requests
+import base64
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "abcdef"
 
 @app.route("/")
 def home():
-    return render_template("index.html", F6=True, F7=False, F13=False, F14=False, F20=False, F21=False)
+    return render_template(
+        "index.html", F6=True, F7=False, F13=False, F14=False, F20=False, F21=False
+    )
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -14,7 +18,27 @@ def login():
     if request.method == "GET":
         return render_template("login.html", login=True)
     else:
-        return request.form
+        url = "http://158.108.182.0:3000/login"
+
+        payload = request.form
+        usrnm = payload["username"]
+        pswd = payload["passwd"]
+        credentials = f"{usrnm}:{pswd}"
+        cred_bytes = credentials.encode('ascii')
+        cred_fin = base64.b64encode(cred_bytes).decode('ascii')
+        headers = {"Authorization": f"Basic {cred_fin}"}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        if (response.status_code == 401):
+            flash("login fail!", "danger")
+            return render_template("login.html")
+        else:
+            json = response.json()
+            resp = make_response("Setting a cookie")
+            resp.set_cookie('token', value = json["token"], httponly = True)
+            flash("login success!", "success")
+            return redirect("/")
 
 
 @app.route("/balance", methods=["PUT", "GET"])
