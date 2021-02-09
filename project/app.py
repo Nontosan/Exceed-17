fefa29d2667bd4871a0cb893a16a897e6983c49d
ChRatchapol <50233981+ChRatchapol@users.noreply.github.com>
2021-02-09 21:37:40 +0700

    finish routing i think

--- project/app.py
+++ project/app.py
@@ -1,10 +1,37 @@
 from flask import Flask, render_template, url_for, request, session, make_response, redirect, flash
+from functools import wraps
 import datetime
 import requests
 import base64
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "abcdef"
+
+def token_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        header = {"Authorization":f"Bearer {request.cookies.get('token')}"}
+        response = requests.get("http://158.108.182.0:3000/", headers=header)
+        print(response.json())
+        if response.json()["message"] == "OK":
+            return func(*args, **kwargs)
+        else:
+            flash("login first!", "danger")
+            return redirect(url_for("home"))
+    return inner
+
+def admin_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        header = {"Authorization":f"Bearer {request.cookies.get('token')}"}
+        response = requests.get("http://158.108.182.0:3000/", headers=header)
+        print(response.json())
+        if response.json()["group"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            flash("You need to be an admin!", "danger")
+            return redirect(url_for("home"))
+    return inner
 
 @app.route("/")
 def home():
@@ -27,6 +54,7 @@ def login():
         cred_bytes = credentials.encode('ascii')
         cred_fin = base64.b64encode(cred_bytes).decode('ascii')
         headers = {"Authorization": f"Basic {cred_fin}"}
+        print(headers)
 
         response = requests.request("POST", url, headers=headers, data=payload)
 
@@ -35,23 +63,28 @@ def login():
             return render_template("login.html")
         else:
             json = response.json()
-            resp = make_response("Setting a cookie")
-            resp.set_cookie('token', value = json["token"], httponly = True)
+            resp = make_response(redirect("/"))
+            print(json)
+            resp.set_cookie('token', json["token"], httponly=True)
             flash("login success!", "success")
-            return redirect("/")
+            return resp
 
 
 @app.route("/balance", methods=["PUT", "GET"])
+@token_required
 def balance():
     return render_template("balance.html", balance=True)
 
 
 @app.route("/lib-store", methods=["PUT", "GET"])
+@token_required
 def lib_store():
     return render_template("lib-store.html", lib_store=True)
 
 
 @app.route("/price-cal")
+@token_required
+@admin_required
 def price_cal():
     return render_template("price-cal.html", price_cal=True)
 
